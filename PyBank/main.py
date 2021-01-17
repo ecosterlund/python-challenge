2f80b901e224c1e47a26a78b283213223032019d
ecosterlund <erikosterlund@comcast.net>
2021-01-17 13:02:59 -0500

    PyBank Complete, missing txt output

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,7 +5,14 @@ import csv
 #telling Python where to access the csv data base to read it
 py_bank_csv = os.path.join('PyBank/resources/budget_data.csv')
 
-
+#lists to store months and profit/losses values
+months_list = []
+profit_losses =[]
+avg_rev_change=[]
+max_rev_change=[]
+min_rev_change =[]
+rev_change =[]
+date = []
 #READ INPUT FILE: budget_data.csv
 #open the file as a csv file to read
 with open(py_bank_csv, 'r') as csv_file:
@@ -13,36 +20,63 @@ with open(py_bank_csv, 'r') as csv_file:
     #skip header
     csv_header = next(csv_reader)
     print(f"CSV Header: {csv_header}")
-    for row in csv_reader:
-        #lists to store months and profit/losses values
-        months_list = row[0]
-        profit = [1]
-        profit_losses =[]
-        greatest_profit_increase = []
-        total_monthly_change = []
+    for line in csv_reader:
         #Calculates total months values (trying to count how many rows are in the list)
-        print('Total Months: '+ str(len(months_list)))
-        #Calculates the Total profit/losses added up from each row
-
-        #Calculates Average Change
-
+        months_list.append(f"{line[0]}")
+        profit_losses.append(f"{line[1]}")
+        date.append(f"{line[0]}")
+    #Calculates Changes in Revenue
+    for i in range(1,len(profit_losses)):
+        profit_losses = [ int(i) for i in profit_losses]
+        rev_change.append(profit_losses[i] - profit_losses[i-1])
+        #Calculates Average Revenue change
+        avg_rev_change = sum(rev_change)/len(rev_change)
         #Calculates Greatest increase in profits
-
+        max_rev_change = max(rev_change)
         #Calculates Greatest Decrease in profits
+        min_rev_change = min(rev_change)
+        #Stores Date values for output (List index out of range error)
+        avg_rev_change_date = str(date[rev_change.index(max(rev_change))+1])
+        max_rev_change_date = str(date[rev_change.index(max(rev_change))+1])
+        min_rev_change_date = str(date[rev_change.index(min(rev_change))+1])
+    #change_index = 
+    print(rev_change.index(max(rev_change)))
+    print(date[24])
 
-#OUTPUT FILE (File IO Error, used to work before updating reader code)
+    #BEGINNING OF TERMINAL OUTPUT
+    #Prints "Financial Analysis"
+    print("Financial Analysis")
+    #Prints ------ to seperate title
+    print("----------------------------")
+    #Prints total number of months 
+    print('Total Months: '+ str(len(months_list)))
+    #Prints Total profit/losses summed up together
+    profit_losses =[ int(x) for x in profit_losses]
+    print("Total: $", sum(profit_losses))
+    #Prints Rev Changes --------------------------------------------------------------MONTHS ARE OFF
+    print("Average Change: " + avg_rev_change_date, avg_rev_change)
+    print("Greatest Increase in Profits: " + max_rev_change_date, max_rev_change)
+    print("Greastest Decrease in Profits: " + min_rev_change_date, min_rev_change)
+
+#print(len(date))
+#print(len(rev_change))
+#for i in zip(date, rev_change):
+    #print(i)
+
+
+#OUTPUT FILE (File IO Error, used to work before updating reader code)--------------------------ASK
 #Specify Output File Path
-#pybank_output = os.path.join('PyBank/analysis/pybank_analysis.csv')
+pybank_output = os.path.join("PyBank", "analysis", "PyBank_analysis.csv" )
 #Open the output file using "write" mode.
-#with open (pybank_output, "w", newline='') as pybank_analysis:
+with open (pybank_output, "w", newline='') as pybank_analysis:
     #Initialize csv.writer
-    #csv_writer = csv.writer(pybank_analysis)
+    csv_writer = csv.writer(pybank_analysis, delimiter = ',')
 
     #write in what the output should look like here
     #"Financial Analysis"
-    #csv_writer.writerow('Financial Analysis')
+    csv_writer.writerow(months_list)
     #Dashes to separate the title
-    #csv_writer.writerow('----------------------------')
+    csv_writer.writerow('----------------------------')
     #Print "Total Months: "
 
     #Print Total:
@@ -52,7 +86,6 @@ with open(py_bank_csv, 'r') as csv_file:
     #Print Greatest Increase in Profits:
 
     #Prints Greatest Decrease in Profits
-
 
 #WHAT THE OUTPUT SHOULD LOOK LIKE IN THE CSV FILE AND TERMINAL
 #Financial Analysis
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,19 @@ import csv
 #telling Python where to access the csv data base to read it
 py_poll_csv = os.path.join('PyPoll/resources/election_data.csv')
 
+#Lists/Variables to store calculations
+voter_id = []
+county = []
+candidate = []
+total_votes = []
+candidate_who_got_votes = []
+percent_votes_won = []
+total_votes_per_candidate = []
+winner = []
+#Defining function to calculate and print precentage of vote won for each candidate and store them in a dictionary?-------NECESSARY?
+#def print_percentages(election_data):
+    #Find total number of votes per candidate
+    #Find percentage of votes received/total
 
 #READ INPUT FILE: budget_data.csv
 #open the file as a csv file to read
@@ -13,15 +26,38 @@ with open(py_poll_csv, 'r') as csv_file:
     #skip header
     csv_header = next(csv_reader)
     print(f"CSV Header: {csv_header}")
-    for row in csv_reader:
-        #Lists/Variables to store calculations
-        #Calculates total number of votes cast
+    for line in csv_reader:
+        voter_id.append(f"{line[0]}")
+        county.append(f"{line[1]}")
+        candidate.append(f"{line[2]}")
+    
+    for i in range(1,len(voter_id)):
         #Complete list of candidates who received votes (conditional to weed out those who didn't get any?)
+        
         #Calculates Percentage of votes each candidate won
-        #Calculates total number of votes each candidate won
+        percent_votes_won = (total_votes_per_candidate/len(voter_id)) * 100))
+        #Calculates total number of votes each candidate won (Use if statement like if name == candidate's name, calculate and store into variable for that candidate)
+
         #Winner of election (conditional for finding who has the most votes)
 
+    #PRINT OUTPUT TO TERMINAL HERE
+    #Print "Election Results"
+    print("Election Results")
+    #Print ----- to seperate title
+    print("-------------------------")
+    #Print Total Votes
+    total_votes = len(voter_id)
+    print("Total Votes: " + str(total_votes))
+    #Print ----- to seperate votes from rest of data
+    print("-------------------------")
+    #Print list of each candidate, vote percentage, and total votes per candidate
 
+    #Print ---- to seperate winner
+    print("-------------------------")
+    #Print Winner's Name
+
+    #Print ---- to end terminal output
+    print("-------------------------")
 
 #OUTPUT FILE (File IO Error, used to work before updating reader code)
 #Specify Output File Path
