497a33d3ad8e7801027dce673effd10aff9dbe46
ecosterlund <erikosterlund@comcast.net>
2021-01-17 17:02:01 -0500

    PyPoll printing Total Votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,13 +3,13 @@ import os
 import csv
 
 #telling Python where to access the csv data base to read it
-py_poll_csv = os.path.join('PyPoll/resources/election_data.csv')
+py_poll_csv = os.path.join("PyPoll", "resources", "election_data.csv")
 
 #Lists/Variables to store calculations
 voter_id = []
 county = []
 candidate = []
-total_votes = []
+total_votes = [len(voter_id)]
 candidate_who_got_votes = []
 percent_votes_won = []
 total_votes_per_candidate = []
@@ -27,15 +27,15 @@ with open(py_poll_csv, 'r') as csv_file:
     csv_header = next(csv_reader)
     print(f"CSV Header: {csv_header}")
     for line in csv_reader:
+        #USE ZIPS TO MAKE FINDING VALUES EASIER?
         voter_id.append(f"{line[0]}")
         county.append(f"{line[1]}")
         candidate.append(f"{line[2]}")
-    
-    for i in range(1,len(voter_id)):
+    #for i in range(1,len(voter_id)):
+        #Calculate
         #Complete list of candidates who received votes (conditional to weed out those who didn't get any?)
-        
         #Calculates Percentage of votes each candidate won
-        percent_votes_won = (total_votes_per_candidate/len(voter_id)) * 100))
+        #percent_votes_won = (total_votes_per_candidate/len(voter_id)) * 100))
         #Calculates total number of votes each candidate won (Use if statement like if name == candidate's name, calculate and store into variable for that candidate)
 
         #Winner of election (conditional for finding who has the most votes)
@@ -50,16 +50,16 @@ with open(py_poll_csv, 'r') as csv_file:
     print("Total Votes: " + str(total_votes))
     #Print ----- to seperate votes from rest of data
     print("-------------------------")
-    #Print list of each candidate, vote percentage, and total votes per candidate
+    #Print list of each candidate, vote percentage, and total votes per candidate (utilize zip folders for finding values?)
 
     #Print ---- to seperate winner
-    print("-------------------------")
+    #print("-------------------------")
     #Print Winner's Name
 
     #Print ---- to end terminal output
-    print("-------------------------")
+    #print("-------------------------")
 
-#OUTPUT FILE (File IO Error, used to work before updating reader code)
+#OUTPUT FILE
 #Specify Output File Path
 #pypoll_output = os.path.join('PyPoll/analysis/pypoll_analysis.csv')
 #Open the output file using "write" mode.
